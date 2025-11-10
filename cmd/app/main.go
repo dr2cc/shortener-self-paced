0245c09941e442c65cb35ab609724e01b34d0101
Constantine <tmail99@gmail.com>
2025-11-10 14:34:54 +0300

    Остановка- да

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -14,6 +14,6 @@ func main() {
 	}
 
 	// Run
-	app := app.NewApp()
+	//app := app.NewApp()
 	app.Run(cfg)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -8,6 +8,7 @@ import (
 	"app/internal/usecase/random"
 	services "app/internal/usecase/shortener"
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -22,16 +23,8 @@ const (
 	envProd  = "prod"
 )
 
-type App struct {
-	httpServer *http.Server
-}
-
-func NewApp() *App {
-	return &App{}
-}
-
 // Run creates objects (via constructors!)
-func (a *App) Run(cfg *config.Config) {
+func Run(cfg *config.Config) {
 	log := setupLogger(cfg.Env)
 	log.Info("init server", slog.String("address", cfg.ServerAddress)) // Помимо сообщения выведем параметр с адресом
 	log.Debug("logger debug mode enabled")
@@ -46,82 +39,60 @@ func (a *App) Run(cfg *config.Config) {
 	// }
 
 	// Use-Case🧹🏦
-	// В данный момент именно service я не создаю. Сложно..
-	// Видимо им можно считать вызов server.NewApp в main
 	randomKey := random.RandomGenerator{}
 	// Создаю сущность этого сервиса
 	service := services.New(randomKey, repo, cfg)
 
 	// HTTP Server🧹🏦
-	// mux := chi.NewRouter()
-	// // middlewares & handlers
-	// v1.Router(mux, cfg, repo, randomKey, log)
-	// a.httpServer = httpserver.New(cfg.HTTPServer.Address, mux, log)
-
-	restServer, err := server.New(cfg, service)
+	httpServer, err := server.New(cfg, service)
 	if err != nil {
 		log.Error("failed to create http server", sl.Err(err))
 		os.Exit(1)
 	}
 
 	// Waiting signal🧹🏦
-	// done := make(chan os.Signal, 1)
-	// signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	// <-done
-	// log.Info("stopping server")
-
-	// // Смысл таймаута был, но сейчас потерян..
-	// ctx := context.Background() //context.WithTimeout(context.Background(), 10*time.Second)
-	// //defer cancel()
-
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
 	wg := &sync.WaitGroup{}
-	wg.Add(2) //nolint:gomnd
+	// TODO: готовлюсь к двум горутинам при добавлении сервера grpc
+	// тогда будет wg.Add(2)
+	wg.Add(1) //nolint:gomnd
 
-	// Старт в двух горутинах
-	go runServer(ctx, wg, restServer, "REST HTTP server", log)
+	go runServer(ctx, wg, httpServer, "HTTP server", log)
+	// // когда добавлю grpc, то добавлю такую строку:
+	// go runServer(ctx, wg, grpcServer, "GRPC server")
+
 	wg.Wait()
 
-	// Shutdown🧹🏦
-	// if err := a.httpServer.Shutdown(ctx); err != nil {
-	// 	log.Error("failed to stop server", sl.Err(err))
-	// 	return
+	// // TODO: close storage
+	// log.Info("trying to shutdown storage")
+	// errClose := repo.Close(context.Background()) //nolint:contextcheck
+	// if errClose != nil {
+	// 	//log.Fatal().Err(errClose)
+	// 	log.Error("the storage was NOT closed correctly", sl.Err(errClose))
+	// 	os.Exit(1)
+	// } else {
+	// 	log.Info("the storage was closed")
 	// }
-	log.Info("trying to shutdown storage gracefully")
-
-	errClose := repo.Close(context.Background()) //nolint:contextcheck
-	if errClose != nil {
-		//log.Fatal().Err(errClose)
-		log.Error("the storage was NOT closed correctly", sl.Err(errClose))
-		os.Exit(1)
-	} else {
-		log.Info("the storage was closed")
-	}
-
-	// TODO: close storage
-
-	log.Info("server stopped")
 }
 
-func runServer(ctx context.Context, wg *sync.WaitGroup, server server.Server, serverName string, log *slog.Logger) {
-	log.Info("%s started", serverName)
+func runServer(ctx context.Context, wg *sync.WaitGroup, server server.Server, servName string, log *slog.Logger) {
+	log.Info(fmt.Sprintf("%s started", servName))
 
 	go func() {
 		<-ctx.Done()
-		log.Info("trying to shutdown %s gracefully", serverName)
-
+		// Shutdown🧹🏦+
+		log.Info(fmt.Sprintf("trying to stop %s", servName))
 		if errShutdown := server.Shutdown(); errShutdown != nil {
-			log.Info("%s server Shutdown: %v", serverName, errShutdown)
+			log.Info("%s server shutdown: %v", servName, errShutdown)
 		} else {
-			log.Info("%s shutted down gracefully", serverName)
+			log.Info(fmt.Sprintf("%s stopped", servName))
 		}
 		wg.Done()
 	}()
 
 	if errRun := server.Run(); errRun != http.ErrServerClosed && errRun != nil {
-		log.Info("%s could not have started: %v", serverName, errRun)
+		log.Info("%s could not have start: %v", servName, errRun)
 	}
 }
 
