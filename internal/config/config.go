21744dec8ced34b9af3e99a771b94a244adfed84
Constantine <tmail99@gmail.com>
2025-11-11 15:06:54 +0300

    iter4

--- internal/config/config.go
+++ internal/config/config.go
@@ -45,10 +45,8 @@ func NewConfig() (*Config, error) {
 	}
 
 	flag.StringVar(&cfg.ServerAddress, "a", "", "host to listen on")
-	flag.StringVar(&cfg.BaseURL, "b", "", "base url")
-	// "pip" или другой, это файл (работает и без расширения),
-	// который будет (должен) находиться в каталоге ./cmd/shortener/
-	// если его не указать- "", то "хранилище" в файле (iter9) не будет создаваться
+	flag.StringVar(&cfg.BaseURL, "b", "", "resulting shortened URL")
+
 	flag.StringVar(&cfg.FilePath, "f", "", "file storage path")
 	flag.StringVar(&cfg.DatabaseDSN, "d", "", "database dsn for connecting to postgres")
 	flag.StringVar(&cfg.ConfigPath, "c", "", "config path")
