cc4167ef62f7e7cebcb389bde894855a11f10130
Constantine <tmail99@gmail.com>
2025-11-11 16:53:08 +0300

    json пока не работает

--- internal/controller/rest/create.go
+++ internal/controller/rest/create.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"app/internal/entity"
+	"encoding/json"
 	"io"
 	"net/http"
 )
@@ -54,6 +55,71 @@ func writeShorteningResult(w http.ResponseWriter, h *Handler, shortURL entity.Sh
 	w.WriteHeader(status)
 	shortenedURL := h.service.FormatShortURL(shortURL.ID)
 	if _, err := w.Write([]byte(shortenedURL)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Назову json post ручку ShortenAPI - обычно при помощи json и создают интерфейс
+func (h *Handler) ShortenAPI(w http.ResponseWriter, r *http.Request) {
+	var v entity.ShortURL
+
+	reader, err := getDecompressedReader(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if errDecode := json.NewDecoder(reader).Decode(&v); errDecode != nil {
+		http.Error(w, "cannot decode json", http.StatusBadRequest)
+		return
+	}
+
+	if v.OriginalURL == "" {
+		http.Error(w, "url required", http.StatusBadRequest)
+		return
+	}
+
+	//userID := h.getUserID(r)
+
+	shortURL, err := h.service.Shorten(r.Context(), v.OriginalURL)
+
+	// var notUniqueErr *storage.NotUniqueURLError
+	// if errors.As(err, &notUniqueErr) {
+	// 	writeShorteningAPIResult(w, h, shortURL, http.StatusConflict)
+	// 	return
+	// }
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// if err = h.addEncryptedUserIDToCookie(&w, userID); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
+
+	writeShorteningAPIResult(w, h, shortURL, http.StatusCreated)
+}
+
+// ShorteningResult is response with shortened url.
+type ShorteningResult struct {
+	Result string `json:"result"`
+}
+
+func writeShorteningAPIResult(w http.ResponseWriter, h *Handler, shortURL entity.ShortURL, status int) {
+	res := ShorteningResult{Result: h.service.FormatShortURL(shortURL.ID)}
+
+	out, err := json.Marshal(res)
+	// out, err := json.Marshal(h.service.FormatShortURL(shortURL.ID))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if _, err = w.Write(out); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
--- internal/entity/short_url.go
+++ internal/entity/short_url.go
@@ -1,11 +1,11 @@
 // "Сущность" будет хранить структуру объекта и его метод (the entities will store the object’s struct and its method)
 package entity
 
-// ShortURL is main entity for system.
+// ShortURL главная сущность (entity) проекта
 type ShortURL struct {
-	// DeletedAt     time.Time
-	OriginalURL string // исходный URL, который был сокращён
-	ID          string // уникальный идентификатор короткого URL.
+	OriginalURL string
+	ID          string // уникальный идентификатор (alias) для OriginalURL
 	// CreatedByID   string
 	// CorrelationID string
+	// DeletedAt     time.Time
 }
