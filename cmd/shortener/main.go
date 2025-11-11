8a3868aa0e405c2d929f2a843c798e96f96158b6
Constantine <tmail99@gmail.com>
2025-11-11 15:20:36 +0300

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,13 +7,12 @@ import (
 )
 
 func main() {
-	// 1️⃣ Configuration
+	// Configuration
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
 
 	// Run
-	//app := app.NewApp()
 	app.Run(cfg)
 }
