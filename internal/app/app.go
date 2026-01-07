58c4c8748226fa7aaf8dbaff3fac95058a5b0c60
Constantine <tmail99@gmail.com>
2026-01-07 14:03:07 +0300

    **

--- internal/app/app.go
+++ internal/app/app.go
@@ -10,6 +10,7 @@ import (
 	"app/internal/repository/pg"
 	"app/internal/server"
 	"app/internal/service"
+	"app/pkg/logger/sl"
 	"context"
 	"errors"
 	"log/slog"
@@ -17,8 +18,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -34,7 +33,14 @@ func Run(cfg *config.Config) {
 	log.Info("init server", slog.String("address", cfg.ServerAddress))
 	log.Debug("logger debug mode enabled")
 
-	// Создаем сущности слоев в обратном порядке!
+	// ❗ В достижении цели “разделения ответственности между всеми слоями приложения” нам помогает “правило
+	// зависимости” (это о круговой диаграмме дяди Боба).
+	// Зависимости направлены только внутрь (внутренний круг ничего не должен знать про внешний
+	// и сущности внутреннего круга не могут обратиться к сущностям внешнего).
+	// ❗ И вот чтобы реализовать “Правило зависимости” мы используем технику dependency injection !
+
+	// Создаем сущности слоев (это three-layered architecture)
+	// в порядке обратном обращению к ним:
 	//
 	// 3️⃣ Repository🧹🏦 (DAL)
 	// Создаем объект хранилища, в соответствии с настройками
@@ -46,17 +52,11 @@ func Run(cfg *config.Config) {
 	// структуры RandomStringGenerator (по сути поведение- метод GenerateIDfromString)
 	// а не интерфейса IDGenerator () (интерфейс служит границей между слоями)
 	randomKey := service.RandomStringGenerator{}
-	// Создаем "сущность" этого сервиса
-	// В чем смысл такой сущности (еще глянуть в обеих чистых архитектурах)?
-	// По моему мнению- чтобы в любом месте проекта были доступны основные методы именно из этй сущности,
-	// а не напрямую (разделение слоев?)
-	// Нет! Это и есть:
-	// 2️⃣ Use case (BL)!
+	// ↑
+	// 2️⃣ Use case (BL - Business Logic Layer, service)
+	// | Здесь внедряем зависимость с repository
 	services := service.NewService(randomKey, repository, cfg)
 	// ↑
-	// |
-
-	// ❗ Начало из todo-app1
 	// 1️⃣ Handler (PL - Presentation Layer, controller)
 	// | Здесь внедряем зависимость с services
 	handlers := handler.NewHandler(services)
@@ -68,11 +68,9 @@ func Run(cfg *config.Config) {
 	// HTTP Server🧹🏦
 	srv := new(server.Server)
 
-	// Отдельная горутина: сервер запускается в своей собственной горутине.
-	// Это необходимо, так как ListenAndServe() является блокирующим вызовом.
-
-	//// ❗Вариант из todo-app1 не проходил тесты.
-	//// Причина:
+	// Запуск и остановку сервера беру из todo-app1
+	// Но он не проходил тесты yp❗
+	// Причина:
 	// 	Функция logrus.Fatalf делает две вещи:
 	// Печатает лог.
 	// Вызывает os.Exit(1).
@@ -83,16 +81,20 @@ func Run(cfg *config.Config) {
 	// Добавил проверку:
 	// if !errors.Is(err, http.ErrServerClosed) {}
 
+	// Отдельная горутина: сервер запускается в своей собственной горутине.
+	// Это необходимо, так как ListenAndServe() является блокирующим вызовом.
 	go func() {
 		if err := srv.Run(cfg.ServerAddress, handlers.InitRoutes(log)); err != nil {
 			// Проверяем, что ошибка НЕ является сигналом о закрытии сервера
 			if !errors.Is(err, http.ErrServerClosed) {
-				logrus.Fatalf("error occured while running http server: %s", err.Error())
+				//logrus.Fatalf("error occured while running http server: %s", err.Error())
+				log.Error("failed to create http server", sl.Err(err))
+				os.Exit(1)
 			}
 		}
 	}()
 
-	logrus.Print("ShortenerApp Started")
+	log.Info("ShortenerApp Started")
 
 	// Graceful shutdown
 	// quit: Это наш "стоп-кран".
@@ -101,76 +103,21 @@ func Run(cfg *config.Config) {
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
 
-	logrus.Print("ShortenerApp Shutting Down")
+	log.Info("ShortenerApp Shutting Down")
 
 	// Корректное завершение (?)
 	// Используем корневой контекст Background
 	if err := srv.Shutdown(context.Background()); err != nil {
-		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+		//logrus.Errorf("error occured on server shutting down: %s", err.Error())
+		log.Error("failed to create http server", sl.Err(err))
+		os.Exit(1)
 	}
 
 	// // TODO: Close storage
 	// if err := db.Close(); err != nil {
 	// 	logrus.Errorf("error occured on db connection close: %s", err.Error())
 	// }
-	// ❗ Конец из todo-app1
-
-	// // // ❌ Переделать вызов сервера!! Как в todo-app1
-	// // 1️⃣ Handler (PL)
-	// router := handler.InitRoutes(services, cfg, log)
-
-	// // HTTP Server🧹🏦
-	// restAPIserver, err := server.New(cfg, router)
-	// if err != nil {
-	// 	log.Error("failed to create http server", sl.Err(err))
-	// 	os.Exit(1)
-	// }
-
-	// // Waiting signal🧹🏦
-	// ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	// wg := &sync.WaitGroup{}
-	// // TODO: готовлюсь к двум горутинам при добавлении сервера grpc
-	// // тогда будет wg.Add(2)
-	// wg.Add(1)
-
-	// go runServer(ctx, wg, restAPIserver, "REST API server", log)
-	// // // когда добавлю gRPC, то добавлю такую строку:
-	// // go runServer(ctx, wg, grpcAPIserver, "gRPC API server", log)
-
-	// wg.Wait()
-
-	// // // TODO: close storage
-	// // log.Info("trying to shutdown storage")
-	// // errClose := repo.Close(context.Background()) //nolint:contextcheck
-	// // if errClose != nil {
-	// // 	//log.Fatal().Err(errClose)
-	// // 	log.Error("the storage was NOT closed correctly", sl.Err(errClose))
-	// // 	os.Exit(1)
-	// // } else {
-	// // 	log.Info("the storage was closed")
-	// // }
 }
-
-// func runServer(ctx context.Context, wg *sync.WaitGroup, server server.Server, servName string, log *slog.Logger) {
-// 	log.Info(fmt.Sprintf("%s started", servName))
-
-// 	go func() {
-// 		<-ctx.Done()
-// 		// Shutdown🧹🏦+
-// 		log.Info(fmt.Sprintf("trying to stop %s", servName))
-// 		if errShutdown := server.Shutdown(); errShutdown != nil {
-// 			log.Info("%s server shutdown: %v", servName, errShutdown)
-// 		} else {
-// 			log.Info(fmt.Sprintf("%s stopped", servName))
-// 		}
-// 		wg.Done()
-// 	}()
-
-// 	if errRun := server.Run(); errRun != http.ErrServerClosed && errRun != nil {
-// 		log.Info("%s could not have start: %v", servName, errRun)
-// 	}
-// }
 
 func choosingStorage(log *slog.Logger, cfg *config.Config) storage.Repository {
 	if cfg.DatabaseDSN != "" {
