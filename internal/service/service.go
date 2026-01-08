3b41fbac940b0033b69fcf63dec65d542ae77dc0
Constantine <tmail99@gmail.com>
2026-01-08 11:40:52 +0300

    //

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,31 +6,25 @@ import (
 	storage "app/internal/repository"
 )
 
+type Service struct {
+	// Сервис сокращения URL (создает ID (shortURL) из url), со своим функционалом
+	Random     IDGenerator
+	repository storage.Repository
+	config     *config.Config
+}
+
+// Вызывается из app
+func NewService(rand IDGenerator, repo storage.Repository, conf *config.Config) *Service {
+	return &Service{
+		Random:     rand,
+		repository: repo,
+		config:     conf,
+	}
+}
+
 // Здесь определены предметные области (доменные зоны).
 // ❗Предметная область это круг задач (сферы реального мира) решаемых приложением.
 // Предметные области этого проекта (по мере добавления):
 // 🔸 сокращение URL.
 //
 // Предотвращение «утечек абстракции» https://habr.com/ru/articles/881918/
-type Service struct {
-	// Сервис сокращения URL (создает ID (shortURL) из url), со своим функционалом
-	Random IDGenerator
-	// ❌ по todo-app, "общаться" с хранилищем правильнее из самого сервиса (пока только Random)
-	repository storage.Repository
-	config     *config.Config
-	//generator  generator.URLGenerator
-}
-
-// Вызывается из app
-func NewService(rand IDGenerator, repo storage.Repository, conf *config.Config) *Service {
-	return &Service{
-		Random: rand,
-		// // ❌ по todo-app вот так создают сервисы приложения
-		// // Создаю новый проект. Назову shortener-todo-app. В нем iter1 и затем iter13(!?)
-		// // В iter1 привожу к todo-app (м.б. и gin использовать?!)
-		// Random:     NewRandomService(repo.Random),
-		repository: repo,
-		config:     conf,
-		//generator:  generator,
-	}
-}
