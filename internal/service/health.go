d7c1f35159c4dfe18e492ccb0bcc99bf712b8ea5
Constantine <tmail99@gmail.com>
2026-01-10 12:56:33 +0300

    ****

--- internal/service/health.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// // HealthCheck проверяет корректность работы выбранного хранилища
-// func (sh *Service) HealthCheck(ctx context.Context) error {
-// 	timeout := 5 * time.Second //nolint:gomnd
-// 	ctx, cancel := context.WithTimeout(ctx, timeout)
-// 	defer cancel()
-// 	return sh.repository.Check(ctx)
-// }
